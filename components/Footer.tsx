c34bf75f95695e36aa38ce0985bda68616f318f9
JesterRomut <lxcmoriah@gmail.com>
2024-11-18 02:35:00 +0800

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,33 @@
+﻿import React from "react";
+import styles from './styles.module.css'
+import {useDarkMode} from "@/lib/use-dark-mode";
+
+import * as config from '@/lib/config'
+
+export function FooterImpl(){
+    const [hasMounted, setHasMounted] = React.useState(false)
+    const { isDarkMode, toggleDarkMode } = useDarkMode()
+    const currentYear = new Date().getFullYear()
+
+    const onToggleDarkMode = React.useCallback(
+        (e) => {
+            e.preventDefault()
+            toggleDarkMode()
+        },
+        [toggleDarkMode]
+    )
+
+    React.useEffect(() => {
+        setHasMounted(true)
+    }, [])
+
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.copyright}>
+                Copyright {currentYear} {config.author}
+            </div>
+        </footer>
+    )
+}
+
+export const Footer = React.memo(FooterImpl)
---
+++ components/NotionPageHeader.tsx
@@ -0,0 +1,94 @@
+﻿import type * as types from 'notion-types'
+import {TiEye} from "react-icons/ti";
+import {TiEyeOutline} from "react-icons/ti";
+import cs from 'classnames'
+import * as React from 'react'
+import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
+
+import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
+import { useDarkMode } from '@/lib/use-dark-mode'
+
+import styles from './styles.module.css'
+import exp from "node:constants";
+
+function ToggleThemeButton() {
+    const [hasMounted, setHasMounted] = React.useState(false)
+    const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+    React.useEffect(() => {
+        setHasMounted(true)
+    }, [])
+
+    const onToggleTheme = React.useCallback(() => {
+        toggleDarkMode()
+    }, [toggleDarkMode])
+
+    return (
+        <div
+            className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
+            onClick={onToggleTheme}
+        >
+            {hasMounted && isDarkMode ? <TiEye /> : <TiEyeOutline />}
+        </div>
+    )
+}
+
+export function NotionPageHeader({
+                                     block
+                                 }: {
+    block: types.CollectionViewPageBlock | types.PageBlock
+}) {
+    const { components, mapPageUrl } = useNotionContext()
+
+
+    return (
+        <header className='notion-header'>
+            <div className='notion-nav-header'>
+                <Breadcrumbs block={block} rootOnly={true}/>
+
+                <div className='notion-nav-header-rhs breadcrumbs'>
+                    <components.PageLink
+                        href="https://jesterromut.notion.site/grand-finale"
+                        key={0}
+                        className={cs(styles.navLink, 'breadcrumb', 'button')}
+                    >
+                        {"View in Notion"}
+                    </components.PageLink>
+                    {navigationLinks
+                        ?.map((link, index) => {
+                            if (!link.pageId && !link.url) {
+                                return null
+                            }
+
+                            if (link.pageId) {
+                                return (
+                                    <components.PageLink
+                                        href={mapPageUrl(link.pageId)}
+                                        key={index}
+                                        className={cs(styles.navLink, 'breadcrumb', 'button')}
+                                    >
+                                        {link.title}
+                                    </components.PageLink>
+                                )
+                            } else {
+                                return (
+                                    <components.Link
+                                        href={link.url}
+                                        key={index}
+                                        className={cs(styles.navLink, 'breadcrumb', 'button')}
+                                    >
+                                        {link.title}
+                                    </components.Link>
+                                )
+                            }
+                        })
+                        .filter(Boolean)}
+
+                    <ToggleThemeButton/>
+
+                    {isSearchEnabled && <Search block={block} title={null}/>}
+                </div>
+            </div>
+        </header>
+    )
+}
